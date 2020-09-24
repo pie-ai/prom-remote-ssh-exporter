0e6da77ece37ccc54ffc01fac1aaa50509b63fd5
Patrick Stricker <git@pa2.de>
2020-09-24 21:51:38 +0200

    cleaned up, host is id

--- src/bin/rust-prometheus-http-exporter.rs
+++ src/bin/rust-prometheus-http-exporter.rs
@@ -1,8 +1,5 @@
 extern crate ssh_prometheus_exporter;
 
-//use clap::{crate_authors, crate_name, crate_version, Arg};
-// use climake::{Argument, CLIMake, DataType};
-
 use ssh_prometheus_exporter::ssh;
 use log::{info, trace};
 //, error};
@@ -11,12 +8,7 @@ use serde::Deserialize;
 use std::env;
 use std::fs::File;
 use std::io::BufReader;
-
 use ssh::LoadAverage;
-//use std::path::PathBuf;
-//use std::convert::TryInto;
-//use std::any::Any;
-//use std::borrow::Borrow;
 
 #[derive(Debug, Clone, Default)]
 struct MyOptions {}
@@ -44,91 +36,6 @@ struct Args {
 // https://www.dev-notes.eu/2020/03/Return-a-Result-Type-from-the-Main-Function-in-Rust/
 #[tokio::main]
 async fn main() -> Result<(), &'static str>{
-    /*
-     let matches = clap::App::new(crate_name!())
-        .version(crate_version!())
-        //.about(crate_description!())
-        .author(crate_authors!("\n"))
-        .arg(
-            Arg::with_name("port")
-                .short("p")
-                .help("exporter port")
-                .default_value("32148")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("verbose")
-                .short("v")
-                .help("verbose logging")
-                .takes_value(false),
-        )
-        .arg(
-            Arg::with_name("endpoints")
-                .short("e")
-                .help("endpoints csv file (identifier,hostname,port,username,password,usage)")
-                .takes_value(true),
-        )
-        .get_matches();
-    let tmp = matches.value_of("endpoints");
-    if matches.is_present("verbose") {
-        env::set_var(
-            "RUST_LOG",
-            format!("folder_size=trace,{}=trace", crate_name!()),
-        );
-    } else {
-        env::set_var(
-            "RUST_LOG",
-            format!("folder_size=info,{}=info", crate_name!()),
-        );
-    }
-
-    let mut endpoints: &str = "foo.txt";//matches.value_of("endpoints").unwrap();
-
-    if matches.is_present("endpoints") {
-        endpoints = tmp.unwrap();
-    }
-*/
-
-
-    /*
-    let arg_endpoints = Argument::new(
-        &['e'],
-        &["endpoints"],
-        Some("endpoints csv file"),
-        DataType::Files,
-    ).unwrap();
-    let arg_verbose =  Argument::new(
-        &['v'],
-        &["verbose"],
-        Some("verbose mode on"),
-        DataType::None,
-    ).unwrap();
-    let args = &[arg_endpoints,arg_verbose];
-
-    let cli = CLIMake::new(args, Some("PA's Rust Prometheus Exporter"), None).unwrap();
-    let args = cli.parse();
-    let mut verbose = false;
-    let mut endpoints: Vec<PathBuf> = Vec::new();
-    for arg in &args {
-        let argument arg.argument;
-        match argument
-        {
-            arg_verbose=> verbose = true,
-            arg_endpoints=>
-                {
-                    match arg.passed_data
-                    {
-                        File => verbose = false
-                    }
-                }
-        }
-
-
-        println!("{}", arg.argument.help.unwrap());
-    }
-
-    println!("Args used: {:#?}", cli.parse());
-    */
     let mut args = pico_args::Arguments::from_env();
     let args = Args {
         help: args.contains(["-h", "--help"]),
@@ -152,25 +59,8 @@ async fn main() -> Result<(), &'static str>{
 
     env_logger::init();
 
-    //info!("using matches: {:?}", &matches);
-
-    //let bind = "8080";//matches.value_of("port").unwrap();
-    //let bind = u16::from_str_radix(&bind, 10).expect("port must be a valid number");
     let addr = ([0, 0, 0, 0], args.port).into();
-
     info!("starting exporter on {}", addr);
-
-    //let endpointsAvailable = matches.is_present("endpoints");
-    //let endpoints = matches.value_of("endpoints");
-    /*if endpoints == ''
-    {
-        error!("specify endpoints file");
-        //std::process::exit(1);
-        return Err("missing endpoints file")
-    }*/
-//    let endpoints: &'static str = args.endpoints.as_str();
-        //let endpoints: &'static str = matches.value_of("endpoints").unwrap();
-    //let endpoints = endpoints.unwrap();
     let endpoints = args.endpoints.clone();
 
     render_prometheus(addr, MyOptions::default(), move |request, options| {
@@ -199,8 +89,6 @@ async fn main() -> Result<(), &'static str>{
             let memory_metric = PrometheusMetric::new("memory", MetricType::Gauge, "memory usage");
             let mut memory_buf = memory_metric.render_header();
 
-
-
             for entry in rdr.deserialize() {
                 let record: Endpoint = entry?;
                 //println!("endpoint: {:?}", record.identifier);
@@ -210,7 +98,7 @@ async fn main() -> Result<(), &'static str>{
                 //println!("processes:\n====\n{}====", processes);
 
                 let mut attributes: Vec<(&str, &str)> = Vec::new();
-                attributes.push(("host", &record.hostname));
+                attributes.push(("host", &record.identifier));
                 
                 let load: LoadAverage = ssh::loadavg(&sess);
                 load_1_buf.push_str(&load_1_metric.render_sample(Some(attributes.as_slice()), load.load1));
@@ -224,7 +112,7 @@ async fn main() -> Result<(), &'static str>{
                     for entry in usage.attributes
                     {
                         let mut usage_attributes: Vec<(&str, &str)> = Vec::new();
-                        usage_attributes.push(("host", &record.hostname));
+                        usage_attributes.push(("host", &record.identifier));
                         usage_attributes.push(("folder",&entry.folder));
                         usage_buf.push_str(&usage_metric.render_sample(Some(usage_attributes.as_slice()),entry.size));
                     }
@@ -236,7 +124,7 @@ async fn main() -> Result<(), &'static str>{
                 {
                     let mut memory_attributes: Vec<(&str, &str)> = Vec::new();
                     memory_attributes.push(("type", &entry.name));
-                    memory_attributes.push(("host", &record.hostname));
+                    memory_attributes.push(("host", &record.identifier));
                     memory_buf.push_str(&memory_metric.render_sample(Some(memory_attributes.as_slice()),entry.size));
                 }
             }
